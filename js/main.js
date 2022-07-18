f42e40a41c5ce11204dda92a99ead25f3ff8dbe1
DimaDzh <dzhartyds@gmail.com>
2022-07-18 13:04:51 +0300

    fixed sprint2

--- js/main.js
+++ js/main.js
@@ -39,9 +39,8 @@ prodcutCardsList.forEach(element => {
 
 
 const acc = document.getElementsByClassName("accordion");
-let i;
 
-for (i = 0; i < acc.length; i++) {
+for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
